00fb86d1b1d244c8e1d5b6ff395947a2e2a20028
noorfarooqy <noor@drongo.vip>
2024-04-17 15:00:49 +0300

    update the main

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 use Noorfarooqy\MpesaLaravel\Services\CustomerToBusinessContract;
 
 
-Route::group(['prefix' => '/api/v1/mpesa/c2b/', 'as' => 'mpesa.c2b.', 'middleware' => 'xml'], function(){
+Route::group(['prefix' => '/api/v1/c2b/', 'as' => 'mpesa.c2b.', 'middleware' => 'xml'], function(){
 
     Route::post('/validate', [CustomerToBusinessContract::class, 'ValidateTransaction'])->name('validate');
     Route::post('/confirm', [CustomerToBusinessContract::class, 'ConfirmTransaction'])->name('validate');
