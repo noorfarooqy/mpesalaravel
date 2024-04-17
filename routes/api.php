b5406193c4d102bdd1b5ce629dcd8a859757c357
noorfarooqy <noor@drongo.vip>
2024-04-17 12:33:21 +0300

    add route confirm

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Noorfarooqy\MpesaLaravel\Services\CustomerToBusinessContract;
 Route::group(['prefix' => '/api/v1/mpesa/c2b/', 'as' => 'mpesa.c2b.', 'middleware' => 'xml'], function(){
 
     Route::post('/validate', [CustomerToBusinessContract::class, 'ValidateTransaction'])->name('validate');
-    Route::post('/validate', [CustomerToBusinessContract::class, 'ConfirmTransaction'])->name('validate');
+    Route::post('/confirm', [CustomerToBusinessContract::class, 'ConfirmTransaction'])->name('validate');
 
 
 });
